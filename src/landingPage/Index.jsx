6b60e53b68e0eed272a3fe000a28647bc0eaf2ad
krishna2699 <krissh.krishnajadhav@gmail.com>
2024-02-17 13:17:10 +0530

    added image and link to login and signup page

--- src/landingPage/Index.jsx
+++ src/landingPage/Index.jsx
@@ -1,42 +1,41 @@
-import React, { useState, useEffect } from "react";
-import { Header } from "./components/Header";
-import { Features } from "./components/Features";
-import { About } from "./components/About";
-import { Services } from "./components/Services";
-import { Gallery } from "./components/Gallery";
-import { Testimonials } from "./components/Testimonials";
-import { Team } from "./components/Team";
-import { Contact } from "./components/Contact";
-import JsonData from "./data/data.json";
-
-import SmoothScroll from "smooth-scroll";
-import "./index.css";
-import { Navigation } from "./components/Navigation";
+import React, { useState, useEffect } from 'react';
+import { Header } from './components/Header';
+import { Features } from './components/Features';
+import { About } from './components/About';
+import { Services } from './components/Services';
+import { Gallery } from './components/Gallery';
+import { Testimonials } from './components/Testimonials';
+import { Team } from './components/Team';
+import { Contact } from './components/Contact';
+import JsonData from './data/data.json';
+import Footer from '../component/Footer';
+import SmoothScroll from 'smooth-scroll';
+import './index.css';
+import { Navigation } from './components/Navigation';
 
 export const scroll = new SmoothScroll('a[href*="#"]', {
-  speed: 1000,
-  speedAsDuration: true,
+     speed: 1000,
+     speedAsDuration: true,
 });
 
 const Index = () => {
-  const [landingPageData, setLandingPageData] = useState({});
-  useEffect(() => {
-    setLandingPageData(JsonData);
-  }, []);
+     const [landingPageData, setLandingPageData] = useState({});
+     useEffect(() => {
+          setLandingPageData(JsonData);
+     }, []);
 
-  return (
-    <div>
-      <Navigation />
-      <Header data={landingPageData.Header} />
-      <Features data={landingPageData.Features} />
-      <About data={landingPageData.About} />
-      <Services data={landingPageData.Services} />
-      <Gallery data={landingPageData.Gallery} />
-      <Testimonials data={landingPageData.Testimonials} />
-      <Team data={landingPageData.Team} />
-      <Contact data={landingPageData.Contact} />
-    </div>
-  );
+     return (
+          <div>
+               <Navigation />
+               <Header data={landingPageData.Header} />
+               <Features data={landingPageData.Features} />
+               <About data={landingPageData.About} />
+               <Services data={landingPageData.Services} />
+               <Team data={landingPageData.Team} />
+               <Contact data={landingPageData.Contact} />
+               <Footer />
+          </div>
+     );
 };
 
 export default Index;
