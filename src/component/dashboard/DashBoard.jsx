8fd8b4d288fd8ca674c5e0aeac26804f8f285180
krishna2699 <krissh.krishnajadhav@gmail.com>
2024-02-19 15:02:35 +0530

    addded: icons

---
+++ src/component/dashboard/DashBoard.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Card from './Card';
+
+const DashBoard = () => {
+     return (
+          <div>
+               <div>hi</div>
+               <div>hi</div>
+               <div></div>
+               <div></div>
+          </div>
+     );
+};
+
+export default DashBoard;
