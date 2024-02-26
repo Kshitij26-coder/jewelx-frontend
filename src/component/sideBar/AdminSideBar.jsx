1af22e2185baded830a5571c49009eafeb7b45ae
rupeshrane <rupeshrane2024@gmail.com>
2024-02-26 14:26:23 +0530

    ..

--- src/component/sideBar/AdminSideBar.jsx
+++ src/component/sideBar/AdminSideBar.jsx
@@ -7,17 +7,35 @@ import DataThresholdingOutlinedIcon from '@mui/icons-material/DataThresholdingOu
 import DiamondOutlinedIcon from '@mui/icons-material/DiamondOutlined';
 import ScaleOutlinedIcon from '@mui/icons-material/ScaleOutlined';
 import FaceRetouchingNaturalOutlinedIcon from '@mui/icons-material/FaceRetouchingNaturalOutlined';
+import EngineeringIcon from '@mui/icons-material/Engineering';
+import InventoryIcon from '@mui/icons-material/Inventory';
+import ReceiptLongIcon from '@mui/icons-material/ReceiptLong';
+import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 
 const AdminSideBar = [
-     { path: '/', icon: <DashboardOutlinedIcon fontSize="large" />, option: 'Dashboard' },
-     { path: '/users', icon: <PeopleOutlinedIcon fontSize="large" />, option: 'Users' },
-     { path: '/contacts', icon: <StoreOutlinedIcon fontSize="large" />, option: 'Subsidiary' },
+     // { path: '/', icon: <DashboardOutlinedIcon fontSize="large" />, option: 'Dashboard' },
+     // { path: '/users', icon: <PeopleOutlinedIcon fontSize="large" />, option: 'Users' },
+     // { path: '/contacts', icon: <StoreOutlinedIcon fontSize="large" />, option: 'Subsidiary' },
+     // { path: '/customers', icon: <FaceRetouchingNaturalOutlinedIcon fontSize="large" />, option: 'Customers' },
+     // { path: '/invoice', icon: <AutoGraphOutlinedIcon fontSize="large" />, option: 'Sales' },
+     // { path: '/faq', icon: <RequestQuoteOutlinedIcon fontSize="large" />, option: 'Accounts' },
+     // { path: '/calendar', icon: <DataThresholdingOutlinedIcon fontSize="large" />, option: 'Metal Stock' },
+     // { path: '/calendar', icon: <DiamondOutlinedIcon fontSize="large" />, option: 'Article Stock' },
+     // { path: '/uom', icon: <ScaleOutlinedIcon fontSize="large" />, option: 'Unit Of Measurement' },
+
+     { path: '/customer-orders', icon: <AddShoppingCartIcon fontSize="large" />, option: 'Orders' },
+     { path: '/user-purchase', icon: <ShoppingBagIcon fontSize="large" />, option: 'Purchase' },
+     { path: '/subsidiary', icon: <StoreOutlinedIcon fontSize="large" />, option: 'Subsidiary' },
+     { path: '/maintenance', icon: <EngineeringIcon fontSize="large" />, option: 'Maintenance' },
      { path: '/customers', icon: <FaceRetouchingNaturalOutlinedIcon fontSize="large" />, option: 'Customers' },
-     { path: '/invoice', icon: <AutoGraphOutlinedIcon fontSize="large" />, option: 'Sales' },
-     { path: '/faq', icon: <RequestQuoteOutlinedIcon fontSize="large" />, option: 'Accounts' },
-     { path: '/calendar', icon: <DataThresholdingOutlinedIcon fontSize="large" />, option: 'Metal Stock' },
-     { path: '/calendar', icon: <DiamondOutlinedIcon fontSize="large" />, option: 'Article Stock' },
+     { path: '/accounts', icon: <RequestQuoteOutlinedIcon fontSize="large" />, option: 'Accounts' },
+     { path: '/metal-stock', icon: <DataThresholdingOutlinedIcon fontSize="large" />, option: 'Metal Stock' },
+     { path: '/article', icon: <DiamondOutlinedIcon fontSize="large" />, option: 'Article Stock' },
      { path: '/uom', icon: <ScaleOutlinedIcon fontSize="large" />, option: 'Unit Of Measurement' },
+     { path: '/metal', icon: <InventoryIcon fontSize="large" />, option: 'Metal' },
+     { path: '/bill', icon: <ReceiptLongIcon fontSize="large" />, option: 'Billing' },
+     { path: '/sales', icon: <AutoGraphOutlinedIcon fontSize="large" />, option: 'Sales' },
 ];
 
 export default AdminSideBar;
