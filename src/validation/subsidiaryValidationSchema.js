18c66865d456b7d20939da56a3050eadf24bcba6
Kshitij26-coder <kshitijsb26@gmail.com>
2024-02-26 22:40:11 +0530

    resolve bug subsidiary update

--- src/validation/subsidiaryValidationSchema.js
+++ src/validation/subsidiaryValidationSchema.js
@@ -4,7 +4,7 @@ export const subsidiaryValidationSchema = Yup.object().shape({
      shopActNumber: Yup.number().required('Shop ACT licence is required'),
      subsidiaryName: Yup.string().required('Subsidiary Name is Required'),
      gstin: Yup.string().required('GST Number is Required'),
-     cashBalance: Yup.number().typeError('Should be a number').required('Cash Balance is Required'),
+     // cashBalance: Yup.number().typeError('Should be a number').required('Cash Balance is Required'),
      address: Yup.string().required('Address is Required'),
      city: Yup.string().required('City is required'),
      logoUrl: Yup.string().required('logo url is required'),
@@ -13,4 +13,3 @@ export const subsidiaryValidationSchema = Yup.object().shape({
           .test('len', 'Pincode must be exactly 6 digits', val => val && val.toString().length === 6)
           .required('Pincode is required'),
 });
-
